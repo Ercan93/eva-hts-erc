1e1b3811241893b6b89dcf7981356d0b4032058b
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-03-02 15:03:52 +0300

    Refactor pagination link styles

--- src/components/AppPagination.vue
+++ src/components/AppPagination.vue
@@ -120,14 +120,14 @@ export default {
           <a
             href="#"
             aria-current="page"
-            class="relative z-10 inline-flex items-center px-4 py-2 text-sm text-white font-semibold focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            class="relative z-10 inline-flex items-center px-4 py-2 text-sm font-semibold focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             v-for="page in pages"
             :key="page"
             @click="handlePageChange(page)"
             :class="{
               'bg-white text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600':
                 currentPage === page,
-              'ring-1 ring-inset ring-gray-300 hover:bg-gray-50 hover:text-black focus:outline-offset-0':
+              'text-white ring-1 ring-inset ring-gray-300 hover:bg-gray-50 hover:text-black focus:outline-offset-0':
                 currentPage !== page,
             }"
             >{{ page }}</a
