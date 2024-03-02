bd049ce51d4fa495b27121616391556a6166cecd
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-03-02 18:15:59 +0300

    Refactor user actions to use environment variable for API URL

--- src/store/modules/user/actions.js
+++ src/store/modules/user/actions.js
@@ -1,15 +1,13 @@
 export default {
   setUser: async ({ commit }) => {
     try {
-      const response = await fetch(
-        "https://65e321ad88c4088649f55e38.mockapi.io/user/user-information",
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+      const url = import.meta.env.VITE_APP_USER_INFO_API_URL;
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
       const data = await response.json();
       commit("SET_USER", data);
