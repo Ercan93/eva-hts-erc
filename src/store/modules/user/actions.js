c6f1a59c53bfb972f074f47b3c373a7d12f58d84
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-03-02 16:46:01 +0300

    Refactor setUser action to fetch user information from API

--- src/store/modules/user/actions.js
+++ src/store/modules/user/actions.js
@@ -1,5 +1,20 @@
 export default {
-  setUser: ({ commit }, user) => {
-    commit("SET_USER", user);
+  setUser: async ({ commit }) => {
+    try {
+      const response = await fetch(
+        "https://65e321ad88c4088649f55e38.mockapi.io/user/user-information",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const data = await response.json();
+      commit("SET_USER", data);
+    } catch (error) {
+      console.error(error);
+    }
   },
 };
