16af3cf9af931c5a39b5160ca18d448e015570b7
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-03-02 22:30:40 +0300

    Refactor auth actions to store token and email in local storage

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -18,14 +18,47 @@ export default {
           "Content-Type": "application/json",
         },
       });
-      const data = await response.json();
+      let data = await response.json();
+      data = data[0].Data;
 
-      commit("SET_TOKEN", data.Data.token);
+      commit("SET_TOKEN", data.AccessToken);
       commit("SET_IS_LOGGED_IN", true);
       commit("SET_EMAIL", payload.email);
+
+      localStorage.setItem("eva-hts-token", data.AccessToken);
+      localStorage.setItem("eva-hts-email", payload.email);
     } catch (error) {
       console.error(error);
       commit("SET_IS_LOGGED_IN", false);
     }
+  },
+
+  checkUserToken: async ({ commit, state }) => {
+    const token = localStorage.getItem("eva-hts-token");
+    const email = localStorage.getItem("eva-hts-email");
+
+    const currentToken = state.token;
+    const currentEmail = state.email;
+
+    if (token && email && token === currentToken && email === currentEmail)
+      return;
+
+    if (token && email) {
+      commit("SET_TOKEN", token);
+      commit("SET_IS_LOGGED_IN", true);
+      commit("SET_EMAIL", email);
+    } else {
+      commit("SET_IS_LOGGED_IN", false);
+    }
+  },
+
+  logout: ({ commit }) => {
+    commit("SET_IS_LOGGED_IN", false);
+    commit("SET_TOKEN", "");
+    commit("SET_EMAIL", "");
+
+    localStorage.removeItem("eva-hts-token");
+    localStorage.removeItem("eva-hts-email");
+    localStorage.removeItem("eva-hts-user");
   },
 };
