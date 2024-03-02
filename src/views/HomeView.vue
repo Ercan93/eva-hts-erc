97029a67dc1a1cc0e878964cb59c60b924a4d937
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-03-02 18:16:23 +0300

    Refactor HomeView.vue: Add user data and sales overview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,9 @@ import BarChart from "@/components/BarChart.vue";
 import AppSelect from "@/components/AppSelect.vue";
 import AppTable from "@/components/AppTable.vue";
 import AppPagination from "@/components/AppPagination.vue";
-
+import { useStore } from "vuex";
+import { ref } from "vue";
+const store = useStore();
 const options = [
   { value: 60, name: "Last 60 Days" },
   { value: 30, name: "Last 30 Days", selected: true },
@@ -11,83 +13,19 @@ const options = [
   { value: 7, name: "Last 7 Days" },
 ];
 
-const tableHeaderOptions = [
-  { value: "daily", name: "Date" },
-  { value: "profit", name: "Profit" },
-  { value: "fba", name: "FBA" },
-  { value: "fbm", name: "FBM" },
-  { value: "wearables", name: "Wearables" },
-  { value: "services", name: "Services" },
-];
+const firstName = ref("");
+const lastName = ref("");
 
-const tableBodyData = [
-  {
-    id: 1,
-    options: [
-      { value: "Tuesday 19-09-2021" },
-      { value: 140 },
-      { value: 16 },
-      { value: 14 },
-      { value: 12 },
-      { value: 20 },
-    ],
-  },
-  {
-    id: 2,
-    options: [
-      { value: "Tuesday 14-09-2021" },
-      { value: 124 },
-      { value: 20 },
-      { value: 14 },
-      { value: 12 },
-      { value: 30 },
-    ],
-  },
-  {
-    id: 3,
-    options: [
-      { value: "Tuesday 11-09-2021" },
-      { value: 112 },
-      { value: 20 },
-      { value: 18 },
-      { value: 14 },
-      { value: 36 },
-    ],
-  },
-  {
-    id: 4,
-    options: [
-      { value: "Tuesday 12-10-2021" },
-      { value: 118 },
-      { value: 24 },
-      { value: 14 },
-      { value: 14 },
-      { value: 36 },
-    ],
-  },
-  {
-    id: 5,
-    options: [
-      { value: "Tuesday 26-10-2021" },
-      { value: 124 },
-      { value: 18 },
-      { value: 16 },
-      { value: 18 },
-      { value: 26 },
-    ],
-  },
-  {
-    id: 6,
-    options: [
-      { value: "Tuesday 02-11-2021" },
-      { value: 130 },
-      { value: 20 },
-      { value: 18 },
-      { value: 14 },
-      { value: 28 },
-    ],
-  },
-];
+const userData = store.getters["user/getUser"];
+if (userData !== null) {
+  firstName.value = userData[0].Data.user.firstName;
+  lastName.value = userData[0].Data.user.lastName;
+}
+
+store.dispatch("sales/setSalesOverview", {
+  email: "email",
+  password: "password",
+});
 </script>
 
 <template>
@@ -95,10 +33,12 @@ const tableBodyData = [
     class="container-fluid bg-gradient-to-r from-blue-950 via-blue-900 to-indigo-950 p-8"
   >
     <div class="flex items-center justify-between pb-8">
-      <p class="text-3xl text-white font-bold">Welcome User,</p>
+      <p class="text-3xl text-white font-bold">
+        Welcome {{ firstName }} {{ lastName }}
+      </p>
       <AppSelect :options="options" />
     </div>
-    <div class="">
+    <div class="bg-white">
       <BarChart />
     </div>
     <div class="mt-4">
