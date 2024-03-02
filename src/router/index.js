1b291e8866de5ab4ac27715f7a835d0924d66570
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-03-02 22:30:53 +0300

    Add authentication check in router beforeEach hook

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import LoginView from "@/views/LoginView.vue";
+import store from "@/store";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,18 @@ const router = createRouter({
       component: LoginView,
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  store.dispatch("auth/checkUserToken");
+  store.dispatch("user/checkUser");
+  
+  let token = localStorage.getItem("eva-hts-token");
+  if (to.name !== "login" && !token) next("/login");
+
+  if (to.name === "login" && token) next("/");
+
+  next();
 });
 
 export default router;
