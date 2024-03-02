60f4701cb6dec58dd5f623e38e654c980693e518
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-03-02 18:16:07 +0300

    Refactor setSalesOverview action to fetch sales data from API

--- src/store/modules/sales/actions.js
+++ src/store/modules/sales/actions.js
@@ -5,7 +5,30 @@ export default {
   setSkuSalesList: ({ commit }, payload) => {
     commit("SET_SKU_SALES_LIST", payload);
   },
-  setSalesOverview: ({ commit }, payload) => {
-    commit("SET_SALES_OVERVIEW", payload);
+  setSalesOverview: (context) => {
+    /* const user = context.rootGetters["user/getUser"];
+       const sellerId = user[0].Data.user.store[0].storeId;
+       const marketplace = user[0].Data.user.store[0].marketplaceName;
+       const body = JSON.stringify({
+          marketplace: "string",
+          sellerId: "string",
+          requestStatus: 0,
+          day: 0,
+          excludeYoYData: true,
+       }); */
+    const url = import.meta.env.VITE_APP_SALES_API_URL;
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        context.commit("SET_SALES_OVERVIEW", data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   },
 };
