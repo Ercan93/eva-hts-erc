7fc95228640b11dbd5f93392cc6d76ab12ae93fe
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-03-02 11:44:22 +0300

    Add StackedBarChart and AppSelect components to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,21 @@
-<script setup></script>
+<script setup>
+import StackedBarChart from "@/components/StackedBarChart.vue";
+import AppSelect from "@/components/AppSelect.vue";
+
+const options = [
+  { value: 60, name: "Last 60 Days" },
+  { value: 30, name: "Last 30 Days", selected: true },
+  { value: 14, name: "Last 14 Days" },
+  { value: 7, name: "Last 7 Days" },
+];
+</script>
 
 <template>
-  <main>
-    <h1>This is an home page</h1>
+  <main class="bg-gradient-to-r from-blue-950 via-blue-900 to-indigo-950">
+    <div class="flex items-center justify-between p-8">
+      <p class="text-3xl text-white font-bold">Daily Sales</p>
+      <AppSelect :options="options" />
+    </div>
+    <StackedBarChart />
   </main>
 </template>
