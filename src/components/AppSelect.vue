f376ca6f48902e0b70c196078f96f2f1adcb4639
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-03-02 11:44:13 +0300

    Add AppSelect component

---
+++ src/components/AppSelect.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="max-w-sm">
+    <label
+      v-if="label"
+      for="countries"
+      class="block mb-2 text-sm font-medium text-gray-900"
+      >{{ label }}</label
+    >
+    <select
+      id="countries"
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg block w-full py-3 px-4"
+    >
+      <option
+        v-for="option in options"
+        :key="option.value"
+        :value="option.value"
+        :selected="option.selected"
+      >
+        {{ option.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script setup>
+import { toRefs } from "vue";
+
+const props = defineProps({
+  options: {
+    type: Array,
+    required: true,
+  },
+  label: {
+    type: String,
+  },
+});
+
+const { options, label } = toRefs(props);
+</script>
