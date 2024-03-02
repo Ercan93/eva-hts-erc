77d62badd2943088ec6a60d144a7acd5a99f7ccb
Ercan Uzunsakal <ercan.uzunsakal@protonmail.com>
2024-03-02 22:30:47 +0300

    Refactor user actions to store user data in local storage

--- src/store/modules/user/actions.js
+++ src/store/modules/user/actions.js
@@ -10,9 +10,21 @@ export default {
       });
 
       const data = await response.json();
-      commit("SET_USER", data);
+      commit("SET_USER", data[0]);
+      localStorage.setItem("eva-hts-user", JSON.stringify(data[0]));
     } catch (error) {
       console.error(error);
+    }
+  },
+
+  checkUser: async ({ commit, state }) => {
+    const user = localStorage.getItem("eva-hts-user");
+    const currentUser = state.user;
+
+    if (user && user === currentUser) return;
+
+    if (user) {
+      commit("SET_USER", JSON.parse(user));
     }
   },
 };
